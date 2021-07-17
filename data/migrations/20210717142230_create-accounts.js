f8dc610fae43cbaa5fb08291929878bfdc953137
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2021-07-17 11:09:14 -0400

    account table schema created

---
+++ data/migrations/20210717142230_create-accounts.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+    // don't forget the return statement
+    return knex.schema.createTable('accounts', tbl => {
+      // creates a primary key called id
+      tbl.increments();
+      // creates a text field called name which is both required and unique
+      tbl.text('userName', 128).unique().notNullable();
+      tbl.text('password', 128).notNullable();
+      tbl.text('profilePic', 128);
+    });
+  };
+  
+  exports.down = function(knex, Promise) {
+    // drops the entire table
+    return knex.schema.dropTableIfExists('accounts');
+  };
