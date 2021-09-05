12d73f5241e8130680521ff1e04b0a7208618c9c
Kenneth Brook <kenneth.brook@hotmail.com>
2021-09-05 13:26:21 -0400

    minor config tweak

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const knex = require('knex');
 const config = require('./knexfile.js');
 
 // we must select the development object from our knexfile
-const db = process.env.NODE_ENV || knex(config.development);
+const db = knex(process.env.NODE_ENV) || knex(config.development);
 
 // export for use in codebase
 module.exports = db;
