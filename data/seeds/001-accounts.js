0ae457bf27d94ffb612004078ba368bb43572d30
Kenneth Brook <kenneth.brook@hotmail.com>
2021-09-05 14:29:25 -0400

    added more info to seed file

--- data/seeds/001-accounts.js
+++ data/seeds/001-accounts.js
@@ -2,7 +2,7 @@ exports.seed = function(knex, Promise) {
   return knex('accounts').truncate()
     .then(function () {
       return knex('accounts').insert([
-        { userName: 'wombat72', password: 'Wizard72' },
+        { userName: 'wombat72', password: 'Wizard72', profilePic: 'not suported yet', fName: 'Ken', lName: 'Brook' },
       ]);
     });
 };
